0f78746a0685fc18b10ab75c8e41b5af21ab8220
Andrey Ostrikov <andrey.ostrikov.bestu@mail.ru>
2021-01-02 23:32:29 +0300

    Добавьте файлы проекта.

---
+++ Lab5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    // Разделим класс program, на несколько
+    // Перенесем метод тестирования в другой файл.
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test();
+            Test2();
+
+
+            Console.ReadKey();
+        }
+    }
+}
